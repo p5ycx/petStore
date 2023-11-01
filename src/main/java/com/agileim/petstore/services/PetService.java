e5f11f97bc10c901d424b3b241d9ef1e7fbfd282
Akash Jangra <jangrakash@gmail.com>
2023-11-01 12:01:45 +0530

    Add service - createPet.

--- src/main/java/com/agileim/petstore/services/PetService.java
+++ src/main/java/com/agileim/petstore/services/PetService.java
@@ -1,4 +1,23 @@
 package com.agileim.petstore.services;
 
+import com.agileim.petstore.model.dto.PetDTO;
+import com.agileim.petstore.model.mapper.PetMapper;
+import com.agileim.petstore.repositories.PetRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PetService {
+
+    private final PetRepository repository;
+
+    private final PetMapper mapper;
+
+    public PetService(PetRepository repository, PetMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public PetDTO createPet(PetDTO petDTO) {
+        return mapper.entityToDto(repository.save(mapper.dtoToEntity(petDTO)));
+    }
 }
