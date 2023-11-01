35afabc40c82f18171b492a1dda7eb23b211fb83
Akash Jangra <jangrakash@gmail.com>
2023-11-01 14:26:30 +0530

    Add more tests .

--- src/test/java/com/agileim/petstore/services/PetServiceTest.java
+++ src/test/java/com/agileim/petstore/services/PetServiceTest.java
@@ -1,5 +1,7 @@
 package com.agileim.petstore.services;
 
+import com.agileim.petstore.exceptions.PetNotFoundException;
+import com.agileim.petstore.model.dto.BasePetDTO;
 import com.agileim.petstore.model.dto.PetDTO;
 import com.agileim.petstore.model.entity.Pet;
 import com.agileim.petstore.model.mapper.PetMapper;
@@ -14,8 +16,10 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 class PetServiceTest {
@@ -39,25 +43,26 @@ class PetServiceTest {
     @Test
     void createPetTest() {
         // Given
-        PetDTO petDTO = createTestPetDTO(null, "Rover");
-        Pet petEntity = createTestPetEntity(1L, "Rover");
+        BasePetDTO basePetDTO = createTestBasePetDTO("Rover");
+        Pet petEntity = createTestPetEntity(null, "Rover");
         PetDTO savedPetDTO = createTestPetDTO(1L, "Rover");
 
-        when(mapper.dtoToEntity(petDTO)).thenReturn(petEntity);
+        when(mapper.basePetDTOToEntity(basePetDTO)).thenReturn(petEntity);
         when(repository.save(petEntity)).thenReturn(petEntity);
         when(mapper.entityToDto(petEntity)).thenReturn(savedPetDTO);
 
         // When
-        PetDTO result = service.createPet(petDTO);
+        PetDTO result = service.createPet(basePetDTO);
 
         // Then
         assertEquals(savedPetDTO, result);
         assertEquals(1L, result.getId());
 
-        verify(mapper, times(1)).dtoToEntity(petDTO);
+        verify(mapper, times(1)).basePetDTOToEntity(basePetDTO);
         verify(repository, times(1)).save(petEntity);
         verify(mapper, times(1)).entityToDto(petEntity);
     }
+
 
     @Test
     void getAllPetsTest() {
@@ -90,11 +95,89 @@ class PetServiceTest {
         verify(mapper, times(1)).entityToDto(petEntity2);
     }
 
+    @Test
+    void updatePetDetailsTest_success() {
+        // Given
+        String petId = "1";
+        Pet existingPetEntity = createTestPetEntity(1L, "Rover");
+        PetDTO updatedPetDto = createTestPetDTO(1L, "Bruno");
+        Pet updatedPetEntity = createTestPetEntity(1L, "Bruno");
+
+        when(repository.findById(1L)).thenReturn(Optional.of(existingPetEntity));
+        when(mapper.dtoToEntity(updatedPetDto)).thenReturn(updatedPetEntity);
+        when(repository.save(updatedPetEntity)).thenReturn(updatedPetEntity);
+        when(mapper.entityToDto(updatedPetEntity)).thenReturn(updatedPetDto);
+
+        // When
+        PetDTO result = service.updatePetDetails(petId, updatedPetDto);
+
+        // Then
+        assertEquals(updatedPetDto, result);
+        assertEquals("Bruno", result.getName());
+
+        verify(repository, times(1)).findById(1L);
+        verify(mapper, times(1)).dtoToEntity(updatedPetDto);
+        verify(repository, times(1)).save(updatedPetEntity);
+        verify(mapper, times(1)).entityToDto(updatedPetEntity);
+    }
+
+    @Test
+    void updatePetDetailsTest_petNotFound() {
+        // Given
+        String petId = "2";
+
+        when(repository.findById(2L)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(PetNotFoundException.class, () -> service.updatePetDetails(petId, new PetDTO()));
+        verify(repository, times(1)).findById(2L);
+    }
+
+
+    @Test
+    void getPetByIdTest() {
+        // Given
+        String id = "1";  // Known pet ID
+        Pet petEntity = createTestPetEntity(1L, "Rover");
+        PetDTO expectedPetDTO = createTestPetDTO(1L, "Rover");
+
+        when(repository.findById(Long.parseLong(id))).thenReturn(Optional.of(petEntity));
+        when(mapper.entityToDto(petEntity)).thenReturn(expectedPetDTO);
+
+        // When
+        PetDTO result = service.getPetById(id);
+
+        // Then
+        assertEquals(expectedPetDTO, result);
+
+        verify(repository, times(1)).findById(Long.parseLong(id));
+        verify(mapper, times(1)).entityToDto(petEntity);
+    }
+
+    @Test
+    void getPetByIdNotFoundTest() {
+        // Given
+        String id = "999";  //random id
+        when(repository.findById(Long.parseLong(id))).thenReturn(Optional.empty());
+
+        // Then
+        assertThrows(PetNotFoundException.class, () -> service.getPetById(id));
+
+        verify(repository, times(1)).findById(Long.parseLong(id));
+    }
+
+
     private Pet createTestPetEntity(Long id, String name) {
         Pet pet = new Pet();
         pet.setId(id);
         pet.setName(name);
         return pet;
+    }
+
+    private BasePetDTO createTestBasePetDTO(String name) {
+        BasePetDTO basePetDTO = new BasePetDTO();
+        basePetDTO.setName(name);
+        return basePetDTO;
     }
 
     private PetDTO createTestPetDTO(Long id, String name) {
