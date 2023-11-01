74e5786d4ba5f05fc1c999cd47c470006345ecec
Akash Jangra <jangrakash@gmail.com>
2023-11-01 13:14:07 +0530

    Add listPets service and Endpoint.

--- src/main/java/com/agileim/petstore/controllers/PetsController.java
+++ src/main/java/com/agileim/petstore/controllers/PetsController.java
@@ -4,10 +4,9 @@ import com.agileim.petstore.model.dto.BasePetDTO;
 import com.agileim.petstore.model.dto.PetDTO;
 import com.agileim.petstore.services.PetService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pets")
@@ -23,5 +22,11 @@ public class PetsController {
     public PetDTO createPets(@RequestBody(required = true)
                              @Valid BasePetDTO basePet) {
         return petsService.createPet(basePet);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<PetDTO> listPets(@RequestParam(value = "limit", defaultValue = "100") Integer limit, @RequestParam(value = "offset", defaultValue = "0") Integer offset) {
+        return petsService.getAllPets(offset, limit);
     }
 }
--- src/main/java/com/agileim/petstore/services/PetService.java
+++ src/main/java/com/agileim/petstore/services/PetService.java
@@ -5,11 +5,16 @@ import com.agileim.petstore.model.dto.PetDTO;
 import com.agileim.petstore.model.entity.Pet;
 import com.agileim.petstore.model.mapper.PetMapper;
 import com.agileim.petstore.repositories.PetRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PetService {
 
+    private static final int MAX_PAGE_SIZE = 100;
     private final PetRepository repository;
 
     private final PetMapper mapper;
@@ -23,5 +28,12 @@ public class PetService {
         Pet petEntity = mapper.basePetDTOToEntity(basePetDTO);
         Pet savedPet = repository.save(petEntity);
         return mapper.entityToDto(savedPet);
+    }
+
+    public List<PetDTO> getAllPets(Integer offset, Integer limit) {
+        if (limit > MAX_PAGE_SIZE) {
+            limit = MAX_PAGE_SIZE;
+        }
+        return repository.findAll(PageRequest.of(offset, limit)).stream().map(mapper::entityToDto).collect(Collectors.toList());
     }
 }
