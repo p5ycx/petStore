b8d898997cf6ee71b0f1bdea787ca61e45ceedfb
Akash Jangra <jangrakash@gmail.com>
2023-11-01 11:55:59 +0530

    Add mapper.

---
+++ src/main/java/com/agileim/petstore/model/mapper/PetMapper.java
@@ -0,0 +1,25 @@
+package com.agileim.petstore.model.mapper;
+
+
+import com.agileim.petstore.model.dto.PetDTO;
+import com.agileim.petstore.model.entity.Pet;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "Spring")
+public interface PetMapper {
+
+    PetMapper INSTANCE = Mappers.getMapper(PetMapper.class);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "tags", target = "tags")
+    PetDTO entityToDto(Pet pet);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "tags", target = "tags")
+    Pet dtoToEntity(PetDTO petDTO);
+
+}
